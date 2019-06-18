fd217cb7d7603f5f55af19864879cf76ba16fca4
Vitalii Shapoval <vitalii_shapoval@ukr.net>
2019-06-18 21:27:13 +0300

    Upload WIP

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -3,7 +3,12 @@ const Router = require('express-promise-router');
 const router = new Router();
 
 router.post('/', async (req, res) => {
-  res.status(200).json({ haha: 'works' });
+  // fs.appendFile('test.txt', 'Sometimes the same is different', (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+
+  res.status(200).json({ haha: 'haha' });
 });
 
 module.exports = router;
